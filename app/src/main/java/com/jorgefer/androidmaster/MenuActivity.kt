475308d4215bcc0b3ec0d3bba2967a4dfe753c33
fer638 <jorgefer638@gmail.com>
2023-05-24 15:57:18 -0600

    Segunda app disenoo de cards interactivass

--- app/src/main/java/com/jorgefer/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/jorgefer/androidmaster/MenuActivity.kt
@@ -5,16 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.jorgefer.androidmaster.fisrtApp.FirstAppActivity
+import com.jorgefer.androidmaster.imccalculator.ImcCalculatorActivity
 
  class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
         btnSaludApp.setOnClickListener{ navigateToSaludApp() }
+        btnIMCApp.setOnClickListener{ navigateToImcApp() }
     }
 
-     fun navigateToSaludApp(){
+     private fun navigateToImcApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+         startActivity(intent)
+     }
+
+     private fun navigateToSaludApp(){
          val intent = Intent(this, FirstAppActivity::class.java)
          startActivity(intent)
      }
