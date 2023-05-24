79198a93802b9beede078ddb3aa26ad5427eb9e0
fer638 <jorgefer638@gmail.com>
2023-05-24 10:12:14 -0600

    Primera app con texto y boton

--- app/src/main/java/com/jorgefer/androidmaster/fisrtApp/FirstAppActivity.kt
+++ app/src/main/java/com/jorgefer/androidmaster/fisrtApp/FirstAppActivity.kt
@@ -1,11 +1,13 @@
 package com.jorgefer.androidmaster.fisrtApp
 
+import android.content.Intent
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.jorgefer.androidmaster.R
 
 class FirstAppActivity : AppCompatActivity() {
@@ -13,9 +15,17 @@ class FirstAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
         val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
-        btnStart.setOnClickListener {
-            Log.i("Jorge", "Boton pulsado")
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
 
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if(name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
         }
         //al arrancar la pantalla
     }
