26b5bf9165d848a3455ac6313df68185338c04ce
fer638 <jorgefer638@gmail.com>
2023-05-24 12:31:12 -0600

    Navegar por menu y pantallas

---
+++ app/src/main/java/com/jorgefer/androidmaster/MenuActivity.kt
@@ -0,0 +1,21 @@
+ package com.jorgefer.androidmaster
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.jorgefer.androidmaster.fisrtApp.FirstAppActivity
+
+ class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        btnSaludApp.setOnClickListener{ navigateToSaludApp() }
+    }
+
+     fun navigateToSaludApp(){
+         val intent = Intent(this, FirstAppActivity::class.java)
+         startActivity(intent)
+     }
+}
